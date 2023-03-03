aa0c64ea7252f5b79e9a9179221a3f4f4d196dfb
Unknown1321 <arenstorffenterprise@gmail.com>
2023-03-03 11:47:01 +0100

    Upper

--- 04._Upper/app.js
+++ 04._Upper/app.js
@@ -1,11 +1,19 @@
 import express from "express";
 const app = express();
 
+import path from "path";
+
 // import jokes from "./util/jokes.js";
 
+/* Our Pages */
 
+app.get("/frontpage", (req, res) => {
+    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"));
+})
 
-
+app.get("/IRLQuests", (req, res) => {
+    res.sendFile(path.resolve("public/pages/IRLQuests/IRLQuests.html"))
+})
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
